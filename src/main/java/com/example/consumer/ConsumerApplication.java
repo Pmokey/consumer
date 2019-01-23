4a24d5c0f89507498784ad41135202e418b7b0e4
wujw <915435367@qq.com>
2019-01-23 16:48:47 +0800

    支持jsp提交

--- src/main/java/com/example/consumer/ConsumerApplication.java
+++ src/main/java/com/example/consumer/ConsumerApplication.java
@@ -2,16 +2,23 @@ package com.example.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 
 @SpringBootApplication
 @EnableDubboConfiguration
-public class ConsumerApplication {
+public class ConsumerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ConsumerApplication.class);
+    }
 
 }
 
--- src/main/java/com/example/consumer/controller/Consumer.java
+++ src/main/java/com/example/consumer/controller/Consumer.java
@@ -5,19 +5,20 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dubbo.service.UserService;
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 @RequestMapping("/")
 public class Consumer {
 
 
 	@RequestMapping("/index")
-    String home() {
-        return "Hello World!";
+	public String home() {
+        return "index";
     }
 }
